2c4cbe0a361e5884338de7c44ad3a010fbd35784
Adrien Audebert <aaudeber@student.42perpignan.fr>
2023-03-14 16:32:35 +0100

    display image and display pixel

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aaudeber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 15:20:00 by aaudeber          #+#    #+#             */
-/*   Updated: 2023/03/13 19:05:19 by aaudeber         ###   ########.fr       */
+/*   Updated: 2023/03/14 16:31:28 by aaudeber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 #include <stdio.h>
 #include <mlx.h>
 #include <mlx_int.h>
+#include <X11/X.h>
+#include <X11/keysym.h>
+
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 400
+#define MLX_ERROR 1
+
 
 typedef struct s_data {
 
@@ -32,20 +39,69 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-int	main(void)
-{	
+int	display_pixel()
+{
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.bits_per_pixel = 4;
-	img.line_length = 500;
-	img.endian= 500;
+	if (mlx == NULL)
+		return (MLX_ERROR);
+	mlx_win = mlx_new_window(mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "Hello world!");
+	if (mlx_win == NULL)
+	{
+		free(mlx_win);
+		return (MLX_ERROR);
+	}
+
+	img.img = mlx_new_image(mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	my_mlx_pixel_put(&img, 1000, 1000, 0x00FF0000);
+
+	my_mlx_pixel_put(&img, 100, 100, 0x00FF0000);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
+	return (0);
+}
+
+int	display_image()
+{
+	void	*mlx;
+	void	*img;
+	void	*img2;
+	void	*mlx_win;
+	char	*relative_path = "./test.xpm";
+	int		img_width;
+	int		img_height;
+	int		t;
+
+	mlx = mlx_init();
+	if (mlx == NULL)
+		return (MLX_ERROR);
+	mlx_win = mlx_new_window(mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "Hello world!");
+	if (mlx_win == NULL)
+	{
+		free(mlx_win);
+		return (MLX_ERROR);
+	}
+	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
+	mlx_put_image_to_window(mlx, mlx_win, img, 0, 0);
+	if (img == NULL)
+	{
+		free(img);
+		return (MLX_ERROR);
+	}
+	mlx_loop(mlx);
+
+	mlx_destroy_window(mlx, mlx_win);
+	mlx_destroy_display(mlx);
+	free(mlx);
+	return (0);
+}
+
+int	main(void)
+{	
+	//display_pixel();
+	display_image();
+	return (0);
 }
