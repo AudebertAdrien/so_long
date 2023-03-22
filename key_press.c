0d4b97e3f90c033e39cb046a22f8279188548785
Adrien Audebert <aaudeber@student.42perpignan.fr>
2023-03-22 14:40:22 +0100

    block wall

--- key_press.c
+++ key_press.c
@@ -6,43 +6,51 @@
 /*   By: aaudeber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 09:12:37 by aaudeber          #+#    #+#             */
-/*   Updated: 2023/03/22 12:01:29 by aaudeber         ###   ########.fr       */
+/*   Updated: 2023/03/22 14:39:27 by aaudeber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	key_press(int keycode, t_vars *vars)
+int	is_wall(t_vars *vars, int line, int column)
 {
-	printf("keycode : %i\n", keycode);
 	int i = vars->character.pos_y;
 	int j = vars->character.pos_x;
 
+	if (vars->map[i + line][j + column] == '1')	
+		return (1);
+	return (0);
+}
+
+void	move_character(t_vars *vars, int line, int column)
+{
+	int i = vars->character.pos_y;
+	int j = vars->character.pos_x;
+
+	if (!is_wall(vars, line, column))
+	{
+		vars->map[i + line][j + column] = vars->map[i][j];
+		vars->map[i][j] = '0';
+	}
+}
+
+int	key_press(int keycode, t_vars *vars)
+{
+	printf("keycode : %i\n", keycode);
 	if (keycode == 65307)
 	{
 		mlx_destroy_display(vars->mlx);
 		mlx_destroy_window(vars->mlx, vars->win);
 		free(vars->mlx);
 	}
+
 	if (keycode == 119)
-	{
-		vars->map[i - 1][j] = vars->map[i][j];
-		vars->map[i][j] = '0';
-	}
+		move_character(vars, -1, 0);
 	if (keycode == 97)
-	{
-		vars->map[i][j - 1] = vars->map[i][j];
-		vars->map[i][j] = '0';
-	}
+		move_character(vars, 0, -1);
 	if (keycode == 115)
-	{
-		vars->map[i + 1][j] = vars->map[i][j];
-		vars->map[i][j] = '0';
-	}
+		move_character(vars, 1, 0);
 	if (keycode == 100)
-	{
-		vars->map[i][j + 1] = vars->map[i][j];
-		vars->map[i][j] = '0';
-	}
+		move_character(vars, 0, 1);
 	return (0);
 }
